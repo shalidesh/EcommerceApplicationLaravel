795c727843987ddb95097a47915d452cd82c3ea3
shalidesh <shalidesh970@gmail.com>
2022-04-16 20:41:45 +0530

    admin category page

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -66,6 +66,9 @@
 														<a title="Dashboard" href="{{route('admin.dashboard')}}">Dashboard</a>
 													</li>
 													<li class="menu-item" >
+														<a title="Categories" href="{{route('admin.categories')}}">Categories</a>
+													</li>
+													<li class="menu-item" >
 														<a title="logout" href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
 													</li>
 													<form id="logout-form" method="post" action="{{route('logout')}}">
